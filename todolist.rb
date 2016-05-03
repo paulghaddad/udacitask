fe664dda8f4f81b0cc03b1f945f7e45a8f888669
Paul Haddad <paulh16@gmail.com>
2016-05-03 07:03:13 -0500

    Create initial TodoList and Item classes from Prep lesson; add new_features.txt

--- todolist.rb
+++ todolist.rb
@@ -1,7 +1,22 @@
 class TodoList
-    # methods and stuff go here
+  attr_reader :list_title, :items
+
+  def initialize(list_title)
+    @title = list_title
+    @items = Array.new
+  end
+
+  def add_item(new_item)
+    item = Item.new(new_item)
+    @items.push(item)
+  end
 end
 
 class Item
-    # methods and stuff go here
+  attr_reader :description, :completed_status
+
+  def initialize(item_description)
+    @description = item_description
+    @completed_status = false
+  end
 end
